497368084161207fe11b95688d6acda493421654
Virkine <virgile.prin@gmail.com>
2019-01-08 16:50:11 +0100

    ADD: some charts in iframe from grafana

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -3,5 +3,17 @@
 @section('title', 'Graph')
 
 @section('content')
-	<h1>Graph</h1>
+	<div class="ui container">
+		<iframe src="http://127.0.0.1:3000/d-solo/DP6VyGPmk/fcity2?refresh=10s&orgId=1&var-capteur=tension&panelId=2&theme=light" width="900" height="300" frameborder="0"></iframe>
+	</div>
+	<div class="ui two column doubling stackable grid container">
+		<div class="column">
+			<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=10s&orgId=1&panelId=6&var-capteur=intensite&theme=light" width="450" height="200" frameborder="0"></iframe>
+		</div>
+		<div class="column">
+			<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=10s&orgId=1&panelId=7&var-capteur=intensite&theme=light" width="450" height="200" frameborder="0"></iframe>
+		</div>
+	</div>
+
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,13 +2,22 @@
 <html>
     <head>
         <title>@yield('title')</title>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"/>
     </head>
     <body>
+        
+        <div class="ui attached stackable menu">
+        <div class="ui container">
+            <a class="item" href="/">
+                <i class="home icon"></i> Home
+            </a>
+            <a class="item" href="/graph">
+                <i class="chart bar icon"></i> Graph
+            </a>
+        </div>
+    </div>
+
         @yield('content')
 
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/graph">Graph</a></li>
-        </ul>
     </body>
 </html>
