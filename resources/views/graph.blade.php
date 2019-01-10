dc445793c0454fc8acdd4dc2ee201809ede88e5d
Antoine-Auffret <antoine@auffret.ovh>
2019-01-10 11:05:28 +0100

    update graph

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
 	<div class="ui container">
-		<iframe src="http://127.0.0.1:3000/d-solo/DP6VyGPmk/fcity2?refresh=10s&orgId=1&var-capteur=intensite&panelId=2&theme=light" width="100%" height="300" frameborder="0"></iframe>
+		<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=30s&orgId=1&var-capteur=vitesse&var-utilisateur=Antoine&var-trajet=All&panelId=2&theme=light" width="100%" height="300" frameborder="0"></iframe>
 	</div>
 	<div class="ui two column doubling stackable grid container">
 		<div class="column">
-			<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=10s&orgId=1&panelId=6&var-capteur=intensite&theme=light" width="450" height="200" frameborder="0"></iframe>
+			<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=30s&orgId=1&var-capteur=vitesse&var-utilisateur=Antoine&var-trajet=All&panelId=6&theme=light" width="100%" height="100" frameborder="0"></iframe>
 		</div>
 		<div class="column">
-			<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=10s&orgId=1&panelId=7&var-capteur=intensite&theme=light" width="450" height="200" frameborder="0"></iframe>
+			<iframe src="http://localhost:3000/d-solo/DP6VyGPmk/fcity2?refresh=30s&orgId=1&var-capteur=vitesse&var-utilisateur=Antoine&var-trajet=All&panelId=7&theme=light" width="100%" height="100" frameborder="0"></iframe>
 		</div>
 	</div>
 
