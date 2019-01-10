907b3242850957d7d44aba94f86013cd0fc8a1d4
Virkine <virgile.prin@gmail.com>
2019-01-10 15:11:14 +0100

    FIX : log for graph

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
+
     public function home()
     {
     	return view('home');
