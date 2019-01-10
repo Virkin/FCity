0657d83e732a0e3e9d6ec4f83c34a61b71034ff6
Virkine <virgile.prin@gmail.com>
2019-01-10 11:19:03 +0100

    FIX : logout

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,7 +8,7 @@ class PagesController extends Controller
 {
     public function home()
     {
-    	return view('welcome');
+    	return view('home');
     }
 
     public function graph()
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,9 +42,6 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('graph') }}">{{ __('Graph') }}</a>
-                        </li>
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -55,6 +52,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('graph') }}">{{ __('Graph') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
