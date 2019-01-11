08d074f5534fbd301bb67b8465bb910a56eed534
Virkine <virgile.prin@gmail.com>
2019-01-11 09:57:01 +0100

    FIX : picture in div

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,11 @@
 <div class="ui raised very padded text container segment">
   <h2 class="ui header">FCity 2</h2>
   <p>Ceci est la page d'accueil de projet FCity 2 qui consiste à la collecte et le traitement de données provenant d'une voiture électrique. Ce projet a été réalisé dans le cadre du projet M1.</p>
-</div>
-<div class="ui container">
+  <div class="ui container">
     <img class="ui centered medium rounded image" src="/img/fcity.jpg">
 </div>
+</div>
+
 
 <!-- <div class="container">
     <div class="row justify-content-center">
