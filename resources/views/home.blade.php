8c7cea878ce8e2d7cd055a93411c93c77ba41aad
Antoine-Auffret <antoine@auffret.ovh>
2019-02-14 17:39:13 +0100

    fix div and navbar from homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="ui very padded segment fluid" style="background-image:url('/img/background-home.jpg'); background-size:cover; background-position: center center; height:100%; width: 100%; top: 0; margin: 0; position: fixed; z-index: 0">
+<div class="ui very padded segment fluid" style="background-image:url('/img/background-home.jpg'); background-size:cover; background-position: center center; height:100vh">
   <div class="ui container" style="margin-top: 10%;">
     <h1 class="ui header centered">Réservez votre voiture électrique dès maintenant</h2>
     <p class="text-center">L'ISEN Brest met à disposition des enseignants un véhicule 100% électrique pour effectuer des trajets au alentour de Brest. Ceci est la page d'accueil de projet FCity 2 qui consiste à la collecte et le traitement de données provenant d'une voiture électrique. Ce projet a été réalisé dans le cadre du projet M1.</p>
@@ -10,6 +10,11 @@
       <button type="button" class="btn btn-lg btn-primary">Réservez maintenant</button>
     </div>
   </div>
+</div>
+<div class="ui container">
+  <h2>Conduire gratuitement une voiture électrique</h2>
+  <p>Conduire gratuitement la voiture électrique de l'ISEN Brest</p>
+  <img class="ui centered medium rounded image" src="/img/fcity.jpg">
 </div>
 
 
@@ -31,6 +36,5 @@
             </div>
         </div>
     </div>
-    <img class="ui centered medium rounded image" src="/img/fcity.jpg">
 </div> -->
 @endsection
