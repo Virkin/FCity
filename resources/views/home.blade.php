a4f34903717729e755757c8b2dfc0ec29b77207f
Antoine-Auffret <antoine@auffret.ovh>
2019-02-14 14:18:10 +0100

    add homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 
-<div class="ui raised very padded text container segment">
-  <h2 class="ui header">FCity 2</h2>
-  <p>Ceci est la page d'accueil de projet FCity 2 qui consiste à la collecte et le traitement de données provenant d'une voiture électrique. Ce projet a été réalisé dans le cadre du projet M1.</p>
+<div class="ui very padded segment fluid" style="background-image:url('/img/background-home.jpg'); background-size:cover; background-position: center center; height:100%;">
   <div class="ui container">
-    <img class="ui centered medium rounded image" src="/img/fcity.jpg">
-</div>
+    <h2 class="ui header centered">Réservez votre voiture électrique dès maintenant</h2>
+    <p class="text-center">L'ISEN Brest met à disposition des enseignants un véhicule 100% électrique pour effectuer des trajets au alentour de Brest. Ceci est la page d'accueil de projet FCity 2 qui consiste à la collecte et le traitement de données provenant d'une voiture électrique. Ce projet a été réalisé dans le cadre du projet M1.</p>
+    <div class="text-center">
+      <button type="button" class="btn btn-lg btn-primary">Réservez maintenant</button>
+    </div>
+  </div>
 </div>
 
 
@@ -29,5 +31,6 @@
             </div>
         </div>
     </div>
+    <img class="ui centered medium rounded image" src="/img/fcity.jpg">
 </div> -->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,10 +77,7 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+     @yield('content')
 </body>
 </html>
