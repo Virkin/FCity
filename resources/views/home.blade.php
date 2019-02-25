afffead1e88162ab284c5d868bfa64b501f4c41a
Antoine-Auffret <antoine@auffret.ovh>
2019-02-25 09:22:14 +0100

    add link to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,45 +8,47 @@
       <h1 class="ui header centered">Réservez gratuitement votre voiture électrique dès maintenant</h2>
       <p class="text-center">L'ISEN Brest met à disposition des enseignants un véhicule 100% électrique connecté pour effectuer des trajets au alentour de Brest.</p>
       <div class="text-center">
-        <button type="button" class="btn btn-lg btn-primary">Réservez maintenant</button>
+        <a class="btn btn-lg btn-primary" href="#" role="button">Réservez maintenant</a>
       </div>
     </div>
   </div>
 </header>
-<div class="ui container">
+<div class="ui container custom-container">
   <div class="row">
     <div class="col-md-6">
       <h2>Conduire gratuitement un véhicule électrique connecté</h2>
       <p>Le véhicule FCity est constitué de nombreux capteurs (vitesse, accélération, tension des batteries, luminosité...) afin d'améliorer votre conduite, garantir votre sécurité et réduire votre consommation électrique.</p>
-      </div>
+      <a class="btn btn-lg btn-primary" href="register" role="button">Créez votre compte maintenant</a>
+    </div>
     <div class="col-md-6">
       <img class="ui centered medium rounded image" src="/img/fcity.jpg">
     </div>
   </div>
 </div>
-<div class="ui container">
+<div class="ui container custom-container">
   <div class="row">
+    <div class="col-md-6">
+      <img class="ui centered medium rounded image" src="/img/fcity.jpg">
+    </div>
     <div class="col-md-6">
       <h2>Analyser votre conduite en temps réel</h2>
       <p>Les données recueillies par le véhicule sont transmises vers le serveur de l'ISEN Brest afin que puissiez les analyser et les comparer.</p>
-      </div>
-    <div class="col-md-6">
-      <img class="ui centered medium rounded image" src="/img/fcity.jpg">
+      <a class="btn btn-lg btn-primary" href="graph" role="button">Visualisez vos données maintenant</a>
     </div>
   </div>
 </div>
-<div class="ui container">
+<div class="ui container custom-container">
   <div class="row">
     <div class="col-md-6">
       <h2>Participer au classement des meilleurs conducteurs</h2>
       <p>L'ISEN Brest propose aux enseignants un classement des conducteurs les plus économe en énergie.</p>
-      </div>
+      <a class="btn btn-lg btn-primary" href="#" role="button">Consultez le classement maintenant</a>
+    </div>
     <div class="col-md-6">
       <img class="ui centered medium rounded image" src="/img/fcity.jpg">
     </div>
   </div>
 </div>
-<div class="ui container">
 
 <!-- <div class="container">
     <div class="row justify-content-center">
