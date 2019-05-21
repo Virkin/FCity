4040043eb19fc988291b4d91b8298014df8e4380
Antoine-Auffret <antoine@auffret.ovh>
2019-05-21 10:36:07 +0200

    add locale fr

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">Connexion</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -14,7 +14,7 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">
                                 <i class="fas fa-envelope"></i>
-                                {{ __('E-Mail Address') }}
+                                Adresse email
                             </label>
 
                             <div class="col-md-6">
@@ -31,7 +31,7 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">
                                 <i class="fas fa-key"></i>
-                                {{ __('Password') }}
+                                Mot de passe
                             </label>
 
                             <div class="col-md-6">
@@ -51,7 +51,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        &nbsp;Se souvenir de moi
                                     </label>
                                 </div>
                             </div>
@@ -60,12 +60,12 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    Connexion
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        Mot de passe oublié
                                     </a>
                                 @endif
                             </div>
